b799b1749c090ecd95220f41d907ddc2b383909b
Cryi <cryi1116@163.com>
2020-04-10 15:18:28 +0800

    未匹配到路由跳转

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Error404 = () => {
+  return (
+    <>
+      <h2>404 NOT A FOUND!</h2>
+      <Link to="/">返回首页</Link>
+    </>
+  );
+};
+
+export default Error404;
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 import Home from '../pages/home';
 import Lesson from '../pages/lesson';
+import Error404 from '../pages/404';
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,9 @@ const AppRouter = () => {
         </Route>
         <Route path="/lesson">
           <Lesson />
+        </Route>
+        <Route path="*">
+          <Error404 />
         </Route>
       </Switch>
     </Router>
